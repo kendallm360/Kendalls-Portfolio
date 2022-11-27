88df1022b058351afc727af75cef53816be112a2
Kendall McGree <mcgreekendall@gmail.com>
2022-11-27 14:13:53 -0600

    update skills in mockData and how they display in the component

--- src/components/MySkills/index.tsx
+++ src/components/MySkills/index.tsx
@@ -1,15 +1,14 @@
 type TSkill = {
-    name: string;
-    logo: string;
-    alt: string;
-}
+  name: string;
+  logo: string;
+  alt: string;
+};
 
-
-const MySkills = ({name, logo, alt }: TSkill): JSX.Element=> {
-    return(<>
-        <img className="SkillLogo" src={logo} alt={alt} />
-        <h3>{name}</h3>
-    </>)
-
-}
-export default MySkills
+const MySkills = ({ name, logo, alt }: TSkill): JSX.Element => {
+  return (
+    <>
+      <img className="SkillLogo" src={logo} alt={`${name}'s ${alt}`} />
+    </>
+  );
+};
+export default MySkills;
