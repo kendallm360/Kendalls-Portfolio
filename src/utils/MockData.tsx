6ae6514b19178791d5d696989cd07c40dee8125e
Kendall McGree <mcgreekendall@gmail.com>
2022-11-27 13:18:26 -0600

    add initial mockData to codebase

---
+++ src/utils/MockData.tsx
@@ -0,0 +1,102 @@
+export const mySkills = [
+    {
+        id: 1,
+        name: "TypeScript",
+        logo: "",
+    },
+    {
+        id: 2,
+        name: "React",
+        logo: "",
+    },
+    {
+        id: 3,
+        name: "JavaScript",
+        logo: "",
+    },
+    {
+        id: 4,
+        name: "HTML5",
+        logo: "",
+    },
+    {
+        id: 5,
+        name: "CSS3",
+        logo: "",
+    },
+    {
+        id: 6,
+        name: "SASS",
+        logo: "",
+    },
+    {
+        id: 7,
+        name: "Cypress",
+        logo: "",
+    },
+    {
+        id: 8,
+        name: "Mocha",
+        logo: "",
+    },
+    {
+        id: 9,
+        name: "Chai",
+        logo: "",
+    },
+    {
+        id: 10,
+        name: "Jest",
+        logo: "",
+    },
+    {
+        id: 11,
+        name: "Next.js",
+        logo: "",
+    },
+    {
+        id: 12,
+        name: "Markdown",
+        logo: "",
+    },
+    {
+        id: 13,
+        name: "React Router",
+        logo: "",
+    },
+    {
+        id: 14,
+        name: "Styled Components",
+        logo: "",
+    },
+    // {
+    //     id: 15,
+    //     name: "Jest",
+    //     logo: "",
+    // },
+    // {
+    //     id: 16,
+    //     name: "Jest",
+    //     logo: "",
+    // },
+    // {
+    //     id: 17,
+    //     name: "Jest",
+    //     logo: "",
+    // },
+    // {
+    //     id: 18,
+    //     name: "Jest",
+    //     logo: "",
+    // },
+    // {
+    //     id: 19,
+    //     name: "Jest",
+    //     logo: "",
+    // },
+    // {
+    //     id: 20,
+    //     name: "Jest",
+    //     logo: "",
+    // },
+]
