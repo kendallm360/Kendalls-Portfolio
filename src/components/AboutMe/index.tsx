e081593cf7a4b379d3cb47408df5dd63835b4257
Kendall McGree <mcgreekendall@gmail.com>
2022-11-27 10:57:13 -0600

    update AboutMe component stylingusing styles from base

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -3,7 +3,7 @@ import fairPicture from "../../images/orangefair.png";
 const AboutMe = () => {
   return (
     <section className="AboutMe">
-      <div className="graphic">
+      <div className="Graphic">
         <img
           className="KendallImage"
           src={fairPicture}
@@ -11,9 +11,9 @@ const AboutMe = () => {
         />
       </div>
       <h2 className="Title">
-        My name is <span className="name">Kendall</span>
+        My name is <span className="Name">Kendall</span>
       </h2>
-      <h3>
+      <h3 className="Summary">
         I am a Navy veteran turned front end software engineer. I have 10 years
         of experience working with diverse teams on time-sensitive projects.
       </h3>
