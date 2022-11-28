91b39fe4ea05c8262d632df048c6faebb814695d
Kendall McGree <mcgreekendall@gmail.com>
2022-11-28 13:37:39 -0600

    update file names to keep consistent naming conventions

--- src/containers/app/index.tsx
+++ src/containers/app/index.tsx
@@ -1,5 +1,5 @@
 import AboutMe from "../../components/AboutMe";
-import NavBar from "../../components/navBar";
+import NavBar from "../../components/NavBar";
 import "./_base.scss";
 
 function App() {
