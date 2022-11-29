3b8ad5e20222d2c73a7a085db4df7711dbe71392
Kendall McGree <mcgreekendall@gmail.com>
2022-11-29 15:46:25 -0600

    create new AboutMe component

---
+++ src/components/AboutMe/index.tsx
@@ -0,0 +1,14 @@
+const AboutMe = () => {
+  return (
+    <section className="AboutMe">
+      <h2 className="SectionHeader">About Me</h2>
+      <h3 className="Overview">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat,
+        massa accumsan convallis consequat, libero ipsum dapibus sem, vitae
+        imperdiet odio ipsum in sem. Praesent consequat blandit tortor, in
+        imperdiet tellus viverra ac.
+      </h3>
+    </section>
+  );
+};
+export default AboutMe;
