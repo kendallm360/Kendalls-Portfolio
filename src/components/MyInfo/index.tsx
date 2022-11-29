bc1a02b20dbd7962f7e089f236134aa36737e1a2
Kendall McGree <mcgreekendall@gmail.com>
2022-11-29 15:21:21 -0600

    update new MyInfo component layout

--- src/components/MyInfo/index.tsx
+++ src/components/MyInfo/index.tsx
@@ -1,31 +1,38 @@
 import "./styles.scss";
-import { mySkills } from "../../utils/MockData";
+// import { mySkills } from "../../utils/MockData";
 import fairPicture from "../../images/orangefair.png";
 
-type TSkill = {
-  id: number;
-  name: string;
-  logo: string;
-  alt: string;
-};
+// type TSkill = {
+//   id: number;
+//   name: string;
+//   logo: string;
+//   alt: string;
+// };
 
-const AboutMe = () => {
-  const skills = mySkills.map((skill: TSkill) => {
-    return (
-      <div key={skill.id} className="Skill">
-        <img
-          key={skill.id}
-          className="SkillLogo"
-          src={skill.logo}
-          alt={`${skill.name}'s ${skill.alt}`}
-        />
-        <h3 className="SkillTitle"> {skill.name}</h3>
-      </div>
-    );
-  });
+const MyInfo = () => {
+  // const skills = mySkills.map((skill: TSkill) => {
+  //   return (
+  //     <div key={skill.id} className="Skill">
+  //       <img
+  //         key={skill.id}
+  //         className="SkillLogo"
+  //         src={skill.logo}
+  //         alt={`${skill.name}'s ${skill.alt}`}
+  //       />
+  //       <h3 className="SkillTitle"> {skill.name}</h3>
+  //     </div>
+  //   );
+  // });
 
   return (
-    <section className="AboutMe">
+    <section className="MyInfo">
+      <h2 className="Title">
+        Hi, my name is <span className="Name">Kendall</span>
+      </h2>
+      <h3 className="Summary">
+        I am a Navy veteran turned front end software engineer. I have 10 years
+        of experience working with diverse teams on time-sensitive projects.
+      </h3>
       <div className="Graphic">
         <img
           className="KendallImage"
@@ -33,16 +40,9 @@ const AboutMe = () => {
           alt="Kendall McGree (creator)"
         />
       </div>
-      <h2 className="Title">
-        My name is <span className="Name">Kendall</span>
-      </h2>
-      <h3 className="Summary">
-        I am a Navy veteran turned front end software engineer. I have 10 years
-        of experience working with diverse teams on time-sensitive projects.
-      </h3>
-      <div className="MySkills">{skills}</div>
+      {/* <div className="MySkills">{skills}</div> */}
     </section>
   );
 };
 
-export default AboutMe;
+export default MyInfo;
