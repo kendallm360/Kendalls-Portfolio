814fb0c738677355e8066b571a3f3e78fe6b4c5a
Kendall McGree <mcgreekendall@gmail.com>
2022-11-29 21:35:22 -0600

    add information to AboutMe component

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -7,11 +7,29 @@ const AboutMe = () => {
           <span className="SectionNumbers">01.</span> About Me
         </span>
       </h2>
+      <ul className="Qualities">
+        {" "}
+        Notable Qualities:
+        <li className="Quality">Self-Starter with a strong work ethic</li>
+        <li className="Quality">
+          Ability to boost morale and confidence in large and small group
+          settings
+        </li>
+        <li className="Quality">
+          Appreciation for consistent feedback that helps support my growth
+          mindset
+        </li>
+        <li className="Quality">
+          Proven ability to develop and maintain professional relationships
+        </li>
+      </ul>
       <h3 className="Overview">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat,
-        massa accumsan convallis consequat, libero ipsum dapibus sem, vitae
-        imperdiet odio ipsum in sem. Praesent consequat blandit tortor, in
-        imperdiet tellus viverra ac.
+        I never aim to be the smartest person in a room but I always want to be
+        among the hardest workers. I am a great team player but culturally I fit
+        best in fast pace environments. Although I am transitioning into this
+        industry I believe I am already equipped with a lot of qualities to be
+        successful. My experience at the NSA helped me become extremely
+        organized, collaborative, and a natural problem solver.
       </h3>
     </section>
   );
