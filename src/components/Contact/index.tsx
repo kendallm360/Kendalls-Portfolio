1803c4893d95fe6c3f456327cbe6662313fd0e9d
Kendall McGree <mcgreekendall@gmail.com>
2022-12-03 12:34:46 -0600

    import and utilize scroll animations in Contact component

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,3 +1,4 @@
+import ScrollAnimation from "react-animate-on-scroll";
 import "../Contact/styles.scss";
 
 const Contact = () => {
@@ -8,19 +9,21 @@ const Contact = () => {
           <span className="SectionNumbers">04.</span> What's Next?
         </span>
       </h2>
-      <h3 className="Beg">Get In Touch</h3>
-      <p className="ActionItem">
-        If you have made it this far I would like to think you are impressed. If
-        thats not the case hopefully you at least see my potential..... if all
-        else fails maybe you just have a big heart and want to give me a chance.
-        Either way I am available for employment so feel free to reach out to me
-        here.
-      </p>
-      <div className="ButtonWrapper">
-        <a href="mailto:mcgreekendall@gmail.com">
-          <button className="EmailMe">Say Hello</button>
-        </a>
-      </div>
+      <ScrollAnimation animateIn="zoomIn" animateOut="zoomOut">
+        <h3 className="Beg">Get In Touch</h3>
+        <p className="ActionItem">
+          If you have made it this far I would like to think you are impressed.
+          If thats not the case hopefully you at least see my potential..... if
+          all else fails maybe you just have a big heart and want to give me a
+          chance. Either way I am available for employment so feel free to reach
+          out to me here.
+        </p>
+        <div className="ButtonWrapper">
+          <a href="mailto:mcgreekendall@gmail.com">
+            <button className="EmailMe">Say Hello</button>
+          </a>
+        </div>
+      </ScrollAnimation>
     </section>
   );
 };
