fb48c729a14415ec33cefc80793575adb1db4293
Kendall McGree <mcgreekendall@gmail.com>
2022-12-03 12:30:38 -0600

    import and utilize scroll animations on Project component

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -1,6 +1,7 @@
 import "../Project/styles.scss";
 import repo from "../../images/githubIcon.png";
 import website from "../../images/websiteIcon.png";
+import ScrollAnimation from "react-animate-on-scroll";
 
 type TProject = {
   name: string;
@@ -20,26 +21,32 @@ const Project = ({
   deployedLink,
 }: TProject) => {
   return (
-    <section className="Projects">
-      <img className="ProjectLogo" src={logo} alt="broken" />
-      <div className="ProjectInfo">
-        <h3 className="ProjectTitle">{name}</h3>
-        <p className="ProjectDescription">{description} Website</p>
-        <p className="TopTechnologies">
-          {topTechnologies[0]}
-          <span className="Divider"> | </span>
-          {topTechnologies[1]}
-        </p>
-        <div className="ProjectLinks">
-          <a className="ProjectLink" href={repoLink}>
-            <img className="ProjectLinkIcon" src={repo} alt="Github logo" />
-          </a>
-          <a className="ProjectLink" href={deployedLink}>
-            <img className="ProjectLinkIcon" src={website} alt="Website logo" />
-          </a>
+    <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutRight">
+      <section className="Projects">
+        <img className="ProjectLogo" src={logo} alt="broken" />
+        <div className="ProjectInfo">
+          <h3 className="ProjectTitle">{name}</h3>
+          <p className="ProjectDescription">{description} Website</p>
+          <p className="TopTechnologies">
+            {topTechnologies[0]}
+            <span className="Divider"> | </span>
+            {topTechnologies[1]}
+          </p>
+          <div className="ProjectLinks">
+            <a className="ProjectLink" href={repoLink}>
+              <img className="ProjectLinkIcon" src={repo} alt="Github logo" />
+            </a>
+            <a className="ProjectLink" href={deployedLink}>
+              <img
+                className="ProjectLinkIcon"
+                src={website}
+                alt="Website logo"
+              />
+            </a>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </ScrollAnimation>
   );
 };
 export default Project;
