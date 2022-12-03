974c7f99a6d121943fc28b52a4366195d46eec68
Kendall McGree <mcgreekendall@gmail.com>
2022-12-03 12:32:25 -0600

    import and utilize scroll animations on AboutMe component

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,3 +1,4 @@
+import ScrollAnimation from "react-animate-on-scroll";
 import "../AboutMe/styles.scss";
 const AboutMe = () => {
   return (
@@ -7,30 +8,32 @@ const AboutMe = () => {
           <span className="SectionNumbers">01.</span> About Me
         </span>
       </h2>
-      <ul className="Qualities">
-        {" "}
-        Notable Qualities:
-        <li className="Quality">Self-Starter with a strong work ethic</li>
-        <li className="Quality">
-          Ability to boost morale and confidence in large and small group
-          settings
-        </li>
-        <li className="Quality">
-          Appreciation for consistent feedback that helps support my growth
-          mindset
-        </li>
-        <li className="Quality">
-          Proven ability to develop and maintain professional relationships
-        </li>
-      </ul>
-      <h3 className="Overview">
-        I never aim to be the smartest person in a room but I always want to be
-        among the hardest workers. I am a great team player but culturally I fit
-        best in fast pace environments. Although I am transitioning into this
-        industry I believe I am already equipped with a lot of qualities to be
-        successful. My experience at the NSA helped me become extremely
-        organized, collaborative, and a natural problem solver.
-      </h3>
+      <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutRight">
+        <ul className="Qualities">
+          {" "}
+          Notable Qualities:
+          <li className="Quality">Self-Starter with a strong work ethic</li>
+          <li className="Quality">
+            Ability to boost morale and confidence in large and small group
+            settings
+          </li>
+          <li className="Quality">
+            Appreciation for consistent feedback that helps support my growth
+            mindset
+          </li>
+          <li className="Quality">
+            Proven ability to develop and maintain professional relationships
+          </li>
+        </ul>
+        <h3 className="Overview">
+          I never aim to be the smartest person in a room but I always want to
+          be among the hardest workers. I am a great team player but culturally
+          I fit best in fast pace environments. Although I am transitioning into
+          this industry I believe I am already equipped with a lot of qualities
+          to be successful. My experience at the NSA helped me become extremely
+          organized, collaborative, and a natural problem solver.
+        </h3>
+      </ScrollAnimation>
     </section>
   );
 };
