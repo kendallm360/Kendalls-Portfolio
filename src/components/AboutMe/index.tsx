621802509d643f9b52f71bee34a53bf089938fda
Kendall McGree <mcgreekendall@gmail.com>
2022-12-05 15:28:19 -0600

    update AboutMe content

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -11,28 +11,43 @@ const AboutMe = () => {
       <ScrollAnimation animateOnce animateIn="fadeInLeft">
         <ul className="Qualities">
           {" "}
-          Notable Qualities:
-          <li className="Quality">Self-Starter with a strong work ethic</li>
+          <span className="Keyword">Notable Qualities:</span>
+          <li className="Quality">
+            <span className="Keyword">Self-Starter</span> with a strong work
+            ethic
+          </li>
           <li className="Quality">
             Ability to boost morale and confidence in large and small group
             settings
           </li>
           <li className="Quality">
-            Appreciation for consistent feedback that helps support my growth
-            mindset
+            Appreciation for consistent feedback that helps support my{" "}
+            <span className="Keyword">growth mindset</span>
           </li>
           <li className="Quality">
             Proven ability to develop and maintain professional relationships
           </li>
         </ul>
-        <h3 className="Overview">
+        <p className="Overview">
           I never aim to be the smartest person in a room but I always want to
-          be among the hardest workers. I am a great team player but culturally
-          I fit best in fast pace environments. Although I am transitioning into
-          this industry I believe I am already equipped with a lot of qualities
-          to be successful. My experience at the NSA helped me become extremely
-          organized, collaborative, and a natural problem solver.
-        </h3>
+          be considered one of the hardest working. Although, I am transitioning
+          into this industry I come with a multitude of transferable skills from
+          previous industries.
+        </p>
+        <p className="Overview">
+          During my career at the NSA being extremely organized and meticulous
+          was imperative in order to be successful. All of my achievements as an
+          Intelligence Analyst stemmed from these qualities and a genuine love
+          for learning.{" "}
+        </p>
+        <p className="Overview">
+          My time in the Navy as an Arabic Linguist highlighted the importance
+          of being a good team player and having strong social skills.
+          Consistently being flexible, positive, and willing to adapt to changes
+          made coworkers/superiors love working with me. I am both easy to coach
+          and a natural leader so I feel like I am a good culture fit for many
+          companies.
+        </p>
       </ScrollAnimation>
     </section>
   );
