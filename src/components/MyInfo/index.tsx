06a674dfd9295ea0cef8eb6cba411b442e48a2c4
Kendall McGree <mcgreekendall@gmail.com>
2022-12-05 18:09:59 -0600

    add animation to MyInfo component

--- src/components/MyInfo/index.tsx
+++ src/components/MyInfo/index.tsx
@@ -1,25 +1,33 @@
 import "./styles.scss";
 import fairPicture from "../../images/kendall_fair.png";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const MyInfo = () => {
   return (
-    <section className="MyInfo">
-      <h2 className="Intro">
-        Hi, my name is <span className="Name">Kendall</span>
-      </h2>
-      <h3 className="Summary">
-        I am a Navy veteran turned front end software engineer. I have 10 years
-        of experience working with diverse teams on{" "}
-        <span className="KeyWord">time-sensitive projects</span>.
-      </h3>
-      <div className="Graphic">
-        <img
-          className="KendallImage"
-          src={fairPicture}
-          alt="Kendall McGree (creator)"
-        />
-      </div>
-    </section>
+    <ScrollAnimation
+      initiallyVisible={true}
+      animateOnce
+      animateIn="fadeInUp"
+      duration={1.5}
+    >
+      <section className="MyInfo">
+        <h2 className="Intro">
+          Hi, my name is <span className="Name">Kendall</span>
+        </h2>
+        <h3 className="Summary">
+          I am a Navy veteran turned front end software engineer. I have 10
+          years of experience working with diverse teams on{" "}
+          <span className="KeyWord">time-sensitive projects</span>.
+        </h3>
+        <div className="Graphic">
+          <img
+            className="KendallImage"
+            src={fairPicture}
+            alt="Kendall McGree (creator)"
+          />
+        </div>
+      </section>
+    </ScrollAnimation>
   );
 };
 
