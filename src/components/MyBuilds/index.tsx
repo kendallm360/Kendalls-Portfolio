54f860c83e0cfe2613dcb7101d821c37ab9a3737
Kendall McGree <mcgreekendall@gmail.com>
2022-12-08 14:40:56 -0600

    create MyBuilds component for alterante Projects view

---
+++ src/components/MyBuilds/index.tsx
@@ -0,0 +1,17 @@
+const MyBuilds = () => {
+  return (
+    <section className="Project">
+      <img
+        className="ProjectLogo"
+        //   src={logo}
+        //   alt={imageAlt}
+      />
+      <div className="ProjectInfo">
+        <h3 className="ProjectTitle">{/* {name} */}</h3>
+        <p className="ProjectDescription">{/* {description} */}</p>
+      </div>
+    </section>
+  );
+};
+
+export default MyBuilds;
