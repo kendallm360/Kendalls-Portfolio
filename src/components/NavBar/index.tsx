56709a1b978cba6fcbdf1587000a592a23f96e1a
Kendall McGree <mcgreekendall@gmail.com>
2022-12-08 21:05:14 -0600

    add git config core.ignorecase false

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,13 @@
+import logo from "../../images/letter-k.png";
+import "./styles.scss";
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      {/* materialUI speed dial */}
+      <img className="Logo" src={logo} alt="Kendall's custom logo" />
+      <button>Contact me</button>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/containers/App/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import AboutMe from "../../components/AboutMe";
+import Contact from "../../components/Contact";
+import Footer from "../../components/Footer";
+import Load from "../../components/Load";
+import MyInfo from "../../components/MyInfo";
+import NavBar from "../../components/NavBar";
+import Skills from "../../components/Skills";
+import Projects from "../Projects";
+import "./_base.scss";
+
+function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <div className="LoadingWrapper">
+          <Load />
+        </div>
+      ) : (
+        <div className="App">
+          <NavBar />
+          <MyInfo />
+          <AboutMe />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
